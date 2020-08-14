a1d185df4573d59611a017895e4ff62631921696
Armin Busatlic <dzarma.busatlic@gmail.com>
2020-08-14 19:52:14 +0200

    Fixing case sensitive issue

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,7 @@
 import React from "react"
 import headerStyles from "./header.module.scss"
 
-import Menu from "../menu/menu"
+import Menu from "../menu/Menu"
 const Header = () => {
   return (
     <header className={headerStyles.main__header}>
